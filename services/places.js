04b70007f7f98b9013c030fe3a51cb17766d65df
nadavgreen <43800418+nadavgreen@users.noreply.github.com>
2019-05-19 16:21:32 -0400

    Places (#12)
    * created routes for places
    
    * created routes for places

--- services/places.js
+++ services/places.js
@@ -2,6 +2,7 @@ const db = require('./db');
 const axios = require('axios');
 const moment = require('moment');
 const PlaceService = {};
+module.exports = {PlaceService};
 
 PlaceService.createPlace = (business_name, categ, sub_categ, addr, city, state, zip, lat, long, stars, review_count, hours, img_url) => {
 	const sql = `INSERT INTO places (business_name, categ, sub_categ, addr, city, state, zip, lat, ling, stars, review_count, hours, img_url)
@@ -9,21 +10,28 @@ PlaceService.createPlace = (business_name, categ, sub_categ, addr, city, state, 
 	return dn.none(sql, { business_name, categ, sub_categ, addr, city, state, zip, lat, long, stars, review_count, hours, img_url })
 };
 
-PlaceService.readPlaceInRadius = (max_lat, min_lat, max_long, min_long) => {
-	const max_lat = parseInt(lat) + 0.00725, min_lat = parseInt(lat) - 0.00725;
-  const max_long = parseInt(long) + 0.00725, min_long = parseInt(long) - 0.00725;
+PlaceService.readPlaceInRadius = (min_lat, max_lat, min_long, max_long) => {
 	return db.any('SELECT * FROM places WHERE lat > $[min_lat] AND lat < $[max_lat] AND long > $[min_long] AND long < $[max_long]', { max_lat, min_lat, max_long, min_long });
 };
 
-PlaceService.readYelpAPI = () => {
-	// put in yelp api
+PlaceService.readYelpAPI = ( offset = 0 ) => {
+	const url = `https://api.yelp.com/v3/businesses/search`
+	const term = `?location=NYC&limit=50&offset=${offset}`
+	const API_Key = `OO3Saz0hvxk-v0QFSvDyL79ElNRxg_BPX0A46BOqWVtdjYN_xRPa4vpvFuPwr6T-wZpzUNUM3uaL_FticZyIhVkKMwm3yFfDY_m7MQ-MxDI4lLQOeTDcaJjPwoXhXHYx`
+	axios.get(`${url}${term}`,
+	{headers: {Authorization: `Bearer ${API_Key}`}})
+	.then(res=>{
+		console.log(res.data.businesses)
+		if(res.data.businesses.length > 1) PlaceService.readYelpAPI((offset+50))
+	})
+	.catch(console.log)
 };
 
 PlaceService.deletePlace = () => {
 	return db.none(`DROP TABLE IF EXISTS places
 			CREATE TABLE places (
-				id SERIAL PRIMARY KEY,
-				business_name VARCHAR NOT NULL,
+			id SERIAL PRIMARY KEY,
+			business_name VARCHAR NOT NULL,
   			categ VARCHAR NOT NULL,
   			sub_categ TEXT [],
   			addr VARCHAR NOT NULL,
@@ -38,4 +46,4 @@ PlaceService.deletePlace = () => {
   			img_url VARCHAR)`)
 };
 
-module.exports = PlaceService;
+PlaceService.readYelpAPI()
