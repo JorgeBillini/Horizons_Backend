09dfb52a30bfe3ca3df67bdea048dbb9471c2a15
nadavgreen <43800418+nadavgreen@users.noreply.github.com>
2019-05-19 17:44:01 -0400

    Places (#13)
    * created routes for places
    
    * created routes for places
    
    * created api call for places services

--- services/places.js
+++ services/places.js
@@ -4,10 +4,10 @@ const moment = require('moment');
 const PlaceService = {};
 module.exports = {PlaceService};
 
-PlaceService.createPlace = (business_name, categ, sub_categ, addr, city, state, zip, lat, long, stars, review_count, hours, img_url) => {
-	const sql = `INSERT INTO places (business_name, categ, sub_categ, addr, city, state, zip, lat, ling, stars, review_count, hours, img_url)
-		VALUES ($[business_name], $[categ], $[sub_categ], $[addr], $[city], $[state], $[zip], $[lat], $[long], $[stars], $[review_count], $[hours], $[img_url])`
-	return dn.none(sql, { business_name, categ, sub_categ, addr, city, state, zip, lat, long, stars, review_count, hours, img_url })
+PlaceService.createPlace = (business_id, business_name, img_url, categories, rating, lat, long, price, address_, city, state_, zip, phone) => {
+	const sql = `INSERT INTO places (business_id, business_name, img_url, categories, rating, lat, long, price, address_, city, state_, zip, phone)
+		VALUES ($[business_id], $[business_name], $[img_url], $[categories], $[rating], $[lat], $[long], $[price], $[address_], $[city], $[state_], $[zip], $[phone])`
+	return db.none(sql, { business_id, business_name, img_url, categories, rating, lat, long, price, address_, city, state_, zip, phone })
 };
 
 PlaceService.readPlaceInRadius = (min_lat, max_lat, min_long, max_long) => {
@@ -21,7 +21,17 @@ PlaceService.readYelpAPI = ( offset = 0 ) => {
 	axios.get(`${url}${term}`,
 	{headers: {Authorization: `Bearer ${API_Key}`}})
 	.then(res=>{
-		console.log(res.data.businesses)
+		const {businesses} = res.data 
+		businesses.forEach( e =>{
+			const {id, name, image_url, categories, rating, price, phone} = e
+			const {latitude, longitude} = e.coordinates
+			const {address1, city, zip_code, state} = e.location
+			PlaceService.createPlace(id, name, image_url, categories, rating, latitude, longitude, address1, city, state, zip_code, phone)
+			.catch(console.log)
+		})
+		return res
+	})
+	.then(res => {
 		if(res.data.businesses.length > 1) PlaceService.readYelpAPI((offset+50))
 	})
 	.catch(console.log)
@@ -31,19 +41,20 @@ PlaceService.deletePlace = () => {
 	return db.none(`DROP TABLE IF EXISTS places
 			CREATE TABLE places (
 			id SERIAL PRIMARY KEY,
+  			business_id VARCHAR NOT NULL,
 			business_name VARCHAR NOT NULL,
-  			categ VARCHAR NOT NULL,
-  			sub_categ TEXT [],
-  			addr VARCHAR NOT NULL,
-  			city VARCHAR NOT NULL,
-  			state VARCHAR NOT NULL,
-  			zip VARCHAR NOT NULL,
-  			lat NUMERIC NOT NULL,
-  			long NUMERIC NOT NULL,
-  			stars NUMERIC,
-  			review_count INT,
-  			hours JSON,
-  			img_url VARCHAR)`)
+			img_url VARCHAR,
+			categories [],
+			rating NUMERIC,
+			lat NUMERIC NOT NULL,
+			long NUMERIC NOT NULL,
+			price VARCHAR, 
+			address_ VARCHAR NOT NULL,
+			city VARCHAR NOT NULL,
+			state_ VARCHAR NOT NULL,
+			zip VARCHAR NOT NULL,
+			phone NUMERIC
+			)`)
 };
 
 PlaceService.readYelpAPI()
