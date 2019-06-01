0b5ed100b8ddcb0563ac9180b4c898a7c95e7eb4
Nadav Green <nadavgreen@Nadavs-MacBook-Pro.local>
2019-06-01 16:30:30 -0400

    fixed error in db where horizon table wouldnt saves

--- services/db.js
+++ services/db.js
@@ -1,3 +1,3 @@
 const pgp = require('pg-promise')({});
-const db = pgp(process.env.DATABASE_URL || 'postgres://localhost/horizons2');
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost/horizons');
 module.exports = db;
