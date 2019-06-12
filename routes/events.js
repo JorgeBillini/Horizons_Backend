06572f6807e0badfddf21e98a0ea1b9eb016511f
qiuyunchen <43826926+qiuyunchen@users.noreply.github.com>
2019-06-11 23:09:58 -0400

    lol im 2 good for this (#41)

--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,48 @@
 const express = require('express');
 const eventRouter = express.Router();
 const EventService = require('../services/events');
+const PastEventService = require('../services/past-events');
 
-eventRouter.get('/',  (req, res, next) => {
-  const { min_lat, min_long, max_lat, max_long } = req.body;
-  EventService.getEventsInRadius(min_lat, min_long, max_lat, max_long)
+eventRouter.post('/', (req, res) =>{
+  const {user_id, name_, description_, category, url_, starts, ends, price, logo, venue, lat, long, capacity} = req.body;
+  EventService.createEvent({user_id, name_, description_, category, url_, starts, ends, price, logo, venue, lat, long, capacity})
+    .then(event =>{
+      res.status(200);
+      res.json({eventCreated: event});
+    })
+    .catch(err =>{
+      res.json({'error': err});
+    })
+});
+
+// get past event service
+eventRouter.get('/past/:user_id', (req, res) =>{
+  const {user_id} = req.params;
+  PastEventService.readEventsByUserId(user_id)
+    .then(data =>{
+      res.status(200);
+      res.json({data});
+    })
+    .catch(err =>{
+      res.json({'error': err});
+    })
+})
+
+eventRouter.get('/:user_id', (req, res) =>{
+  const {user_id} = req.params;
+  EventService.getCurrentEventsByUserId(user_id)
+    .then(data =>{
+      res.status(200);
+      res.json({data});
+    })
+    .catch(err =>{
+      res.json({'error': err});
+    })
+})
+
+eventRouter.get('/',  (req, res) => {
+  const { min_lat, min_long, max_lat, max_long } = req.query;
+  EventService.getEventsInRadius(min_lat, max_lat, min_long, max_long)
     .then(data => {
       res.status(200);
       res.json({'data':data});
@@ -13,29 +51,15 @@ eventRouter.get('/',  (req, res, next) => {
       res.json({'error':err});
     });
 });
-eventRouter.post('/',(req,res)=>{
-  /*
-  name_ VARCHAR NOT NULL,
-  description_ VARCHAR,
-  url_ VARCHAR NOT NULL, 
-  starts TIMESTAMP NOT NULL,
-  ends TIMESTAMP NOT NULL,
-  price VARCHAR NOT NULL,
-  logo VARCHAR,
-  venue VARCHAR NOT NULL,
-  lat NUMERIC,
-  long NUMERIC,
-  capacity INT NOT NULL
-  */
-})
-eventRouter.get('/u', (req, res, next) => {
+
+eventRouter.get('/u', (req, res) => {
   EventService.updateEvents()
   .then(() => {
     res.json({success: true});
   })
   .catch(err => {
-    console.log(err.toString());
-    res.json({'error': err.toString()});
+    console.log(err.ToString());
+    res.json({'error': err.ToString()});
   });
 });
 
